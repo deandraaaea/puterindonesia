f7541fa9236d4ce260e08b35b73ec4d63edd5907
deandraaaea <mochdeandra22@gmail.com>
2024-07-04 21:47:00 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,6 +204,40 @@
     }
   });
 
+  new Swiper('.updatekegiatan', {
+    speed: 400,
+    loop: false,
+    // centeredSlides: true,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false
+    // },
+    // slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      575: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
   /**
    * Initiate gallery lightbox 
    */
